ebd25c908825096bcf39b6e4b1d450b264b824d7
JerryEsSi <gerardo.estrada.908@outlook.com>
2023-09-13 14:45:57 -0600

    avance socialpage

--- front-end/src/componetns/SocialPage.jsx
+++ front-end/src/componetns/SocialPage.jsx
@@ -6,7 +6,84 @@ export function SocialPage() {
 
   return <div className="social">
 
-    <h1>asdfasdfa</h1>
+<div>
+      <article className='contart contartinp'>
+        <div className='contaline'>
+          <div className='contimgpar'>
+            <img src="assets/baner_superior.png"alt="Imagen baner" className='imagenbaener'/>
+          </div>
+        </div>
+      </article>
+</div>
+
+<div>
+      <article className='contart contartinp'>
+        <div className='contaline'>
+          <div className='conttextinp'>
+            <p className='parrafou textinp'> Actuar en favor de la economía de las amas de casa y los sectores vulnerables. 
+              Marcamos la diferencia en la vida de las personas que usan nuestra plataforma ofreciendo flexibilidad y buen servicio.</p>
+          </div>
+        </div>
+      </article>
+    </div>
+
+    <div>
+      <article className='contart contartinp'>
+        <div className='contaline'>
+          <div className='conttextinpar'>
+          <h3 className='titleless textinpar'>Mas que solo una plataforma</h3>
+            <p className='parrafou textinpar'> Nuestra plataforma se enfoca principalmente en el apoyo a las amas de casa y madres solteras, 
+            permientiendoles que puedan poner a la venta su comida recien prearada para ganar dinero sin afectar sus actividades diarias.
+            </p>
+          </div>
+        </div>
+      </article>
+    </div>
+
+    <div>
+      <article className='contart contartinp'>
+        <div className='contaline'>
+          <div className='conttextpar'>
+            <h2 className='titleless textinpar'>Este año hemos aportado</h2>
+            <h2 className='titleless textinpar'>$856 MXN</h2>
+            <p className='parrafou textinpar'> Ayudando a:
+            <br />
+            2
+            <br />
+            amas de casa</p>
+          </div>
+          <div className='contimgpar'>
+            <img src="assets/gente.png" alt="imagen de personas" className='imgrecept'/>
+          </div>
+        </div>
+      </article>
+    </div>
+
+
+    <div>
+      <article className='contart contartinp'>
+        <div className='contaline'>
+          <div className='conttextinpar'>
+          <h3 className='titleless textinpar'>Ayudando a sectores vulnerables</h3>
+            <p className='parrafou textinpar'> Empleamos un modelo de repartidores fexibles para que los estudiantes y 
+                                              gente de escasos recursos puedan participar. 
+                                               Asi pueden usar su tiempo libre para generar un ingreso extra
+
+            </p>
+          </div>
+        </div>
+      </article>
+    </div>
+    <div>
+      <article className='contart contartinpar'>
+        <div className='contaline'>
+
+          <div className='contimginpar'>
+            <img src="assets/baner_inferior.png" alt="imagen de personas" className='imgrecept'/>
+          </div>
+        </div>
+      </article>
+    </div>
 
   </div>
 }
